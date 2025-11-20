3e7b0b6deb50d16d69a556d8169587a5d1a521db
Ratsoi Oleksandr <san4es772@gmail.com>
2025-11-21 00:15:54 +0200

    lab 2

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        HumanBmi human = new HumanBmi(80, 1.52);
-
-        System.out.println("BMI: " + human.getBmi());
-        System.out.println("Status: " + human.getStatus());
-    }
-}
